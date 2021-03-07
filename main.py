55602c6d7f0519abf7159bbd760f52e7baa14666
Tetr4 <tetr4k@gmail.com>
2021-03-07 21:44:49 +0000

    Flesh add remove_candidate and remove_winner

--- main.py
+++ main.py
@@ -5,6 +5,27 @@ class WeightedBallot(NamedTuple):
     weight: float
     ranking: tuple[str]
 
+    def remove_candidate(self, candidate):
+        return WeightedBallot(self.weight, tuple(c for c in self.ranking if c != candidate))
+
+    def remove_winner(self, winner, weight_frac):
+        if len(self.ranking) > 0 and self.ranking[0] == winner:
+            return WeightedBallot(self.weight * weight_frac, self.ranking).remove_candidate(winner)
+        return self
+
+class WeightedBallots(NamedTuple):
+    ballots: tuple[WeightedBallot]
+
+    def remove_candidate(self, candidate):
+        return WeightedBallots(
+            tuple(ballot.remove_candidate(candidate) for ballot in self.ballots)
+        )
+
+    def remove_winner(self, winner, weight_frac):
+        return WeightedBallots(
+            tuple(ballot.remove_winner(winner, weight_frac) for ballot in self.ballots)
+        )
+
 def get_droop_quota(seats: int, ballot_count: int) -> float:
     return ballot_count / (seats + 1)
 
@@ -14,6 +35,15 @@ def tally_first_place_votes(candidates: tuple[str], ballots) -> dict[str, int]:
     tally = {candidate: first_place_ballots.count(candidate) for candidate in candidates}
     tally = {candidate: tally[candidate] for candidate in sorted(tally, key= lambda x: tally[x], reverse=True)}
     return tally
+
+def remove_candidate(candidate: str, ballots: tuple[WeightedBallot]):
+    '''Remove a candidate from all ballots in a tuple of WeightedBallot'''
+    return tuple(ballot.remove_candidate(candidate) for ballot in ballots)
+
+def remove_winner(winner: str, vote_frac: int, ballots: tuple[WeightedBallot]):
+    '''remove_candidate but also lower the weight for ballots for candidate'''
+    return tuple(ballot.remove_winner(winner, vote_frac) for ballot in ballots)
+
 
 def stv(seats: int, candidates: tuple[str], ballots: tuple[tuple[str]], quota=get_droop_quota):
     ballots = tuple(WeightedBallot(1, ballot) for ballot in ballots)
@@ -34,7 +64,14 @@ def stv(seats: int, candidates: tuple[str], ballots: tuple[tuple[str]], quota=ge
     tally = tally_first_place_votes(candidates, ballots)
     quota = quota(seats, sum(candidate_tally for candidate_tally in tally.values()))
 
-    print(tally)
+    if max(tally.values()) > quota:
+        pass
+    else:
+        pass
+
+    nb = remove_winner("Candidate 7", 0.5, ballots)
+    for b in nb[:5]:
+        print(b.weight, b.ranking)
     
 
 def main():
